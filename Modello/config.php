198ef31f74787207896230c8703d47e410dca3f6
Christian <christian2tv@gmail.com>
2017-09-06 22:37:39 +0200

    Update config.php

--- Modello/config.php
+++ Modello/config.php
@@ -2,7 +2,7 @@
 
 $db_host = "localhost";
 $db_user = "root";
-$db_password = "sciences";
+$db_password = "";
 $db_name = "eventi";
 $DB = new mysqli($db_host, $db_user, $db_password, $db_name);
 if($DB->connect_error) die("Connessione al database non riuscita: " . $DB->connect_error);
